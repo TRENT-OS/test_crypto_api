33d04cf127c87ff21c40695f5c65491ba0808dbb
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-23 09:52:21 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- components/Tests/src/test_OS_Crypto.c
+++ components/Tests/src/test_OS_Crypto.c
@@ -6,7 +6,11 @@
  *
  * @brief top level test for the crypto API and the key store API
  *
- * Copyright (C) 2019, HENSOLDT Cyber GmbH
+ * Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #include "OS_Crypto.h"
